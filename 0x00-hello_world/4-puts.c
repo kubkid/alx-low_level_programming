38b34ce43e6fbdfb912315965cd06c09b56cf9e7
kubkid <yoniyangi6@gmail.com>
2023-07-14 16:54:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (succes)
- */n
-  int main(void)
+ * Return: Always 0 (sucess)
+ */
+int main(void)
 {
-       	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
